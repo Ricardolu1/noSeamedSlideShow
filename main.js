7b97f02d7ba5fc59d95c1cbef442bc1540a1ab4a
ycy <568082704@qq.com>
2019-07-25 20:24:30 +0800

    没给3加enter有hug

--- main.js
+++ main.js
@@ -1,37 +1,25 @@
 $(".images>img:nth-child(1)").addClass("current")
 $(".images>img:nth-child(2)").addClass("enter")
 let n=1
-setTimeout(() => {
-  if(n>3){
-    n=n%3
-    if(n===0)
-    n=3
-  }//n的值是1，2，3，不会是别的
-  $(`.images>img:nth-child(${n})`).removeClass().addClass("leave")
+setInterval(() => {
+  $(`.images>img:nth-child(${judgeN(n)})`).removeClass().addClass("leave")
   .one('transitionend',function(x) {
-    $('x.currentTarget').removeClass().addClass('enter')
+    $(x.currentTarget).removeClass().addClass('enter')
   })
-  $(`.images>img:nth-child(${n+1})`).removeClass().addClass("current")
+  $(`.images>img:nth-child(${judgeN(n+1)})`).removeClass().addClass("current")
   n+=1
-}, 2000)
+}, 3000)
 
-setTimeout(() => {
-  $(".images>img:nth-child(2)").removeClass().addClass("leave")
-  .one('transitionend',function(x) {
-    $('x.currentTarget').removeClass().addClass('enter')
-  })
-
-  $(".images>img:nth-child(3)").removeClass().addClass("current")
-}, 4000)
-
-setTimeout(() => {
-  $(".images>img:nth-child(3)").removeClass().addClass("leave")
-  .one('transitionend',function(x) {
-    $('x.currentTarget').removeClass().addClass('enter')
-  })
-
-  $(".images>img:nth-child(1)").removeClass().addClass("current")
-}, 6000)
+//我们要写一个函数，保证n的值是1，2，3不会是别的
+function judgeN(n) {
+  if (n>3) {
+    n=n%3
+    if (n===0){
+      n=3
+    }
+  }
+  return n
+}
 
 
 
