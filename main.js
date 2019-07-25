351543fe1dc0d023cfa1e916bf2608e6e60c1d80
ycy <568082704@qq.com>
2019-07-25 19:44:56 +0800

    要改循环了，先存一下

--- main.js
+++ main.js
@@ -1,47 +1,91 @@
+$(".images>img:nth-child(1)").addClass("current")
+$(".images>img:nth-child(2)").addClass("enter")
+let n=1
 setTimeout(() => {
-  $('.images>img:nth-child(1)').css({
-    transform:'translateX(-100%)'
+  if(n>3){
+    n=n%3
+    if(n===0)
+    n=3
+  }//n的值是1，2，3，不会是别的
+  $(`.images>img:nth-child(${n})`).removeClass().addClass("leave")
+  .one('transitionend',function(x) {
+    $('x.currentTarget').removeClass().addClass('enter')
   })
-  $('.images>img:nth-child(2)').css({
-    transform:'translateX(-100%)'
-  })
-  $('.images>img:nth-child(1)').one('transitionend',function(x) {
-    $(x.currentTarget).addClass('right').css({transform:'none'})
-  })
-}, 2000);
+  $(`.images>img:nth-child(${n+1})`).removeClass().addClass("current")
+  n+=1
+}, 2000)
 
 setTimeout(() => {
-  $('.images>img:nth-child(2)').css({
-    transform:'translateX(-200%)'
+  $(".images>img:nth-child(2)").removeClass().addClass("leave")
+  .one('transitionend',function(x) {
+    $('x.currentTarget').removeClass().addClass('enter')
   })
-  $('.images>img:nth-child(3)').css({
-    transform:'translateX(-100%)'
-  })
-  $('.images>img:nth-child(2)').one('transitionend',function(x) {
-    $(x.currentTarget).addClass('right').css({transform:'none'})
-  })
-}, 4000);
+
+  $(".images>img:nth-child(3)").removeClass().addClass("current")
+}, 4000)
 
 setTimeout(() => {
-  $('.images>img:nth-child(3)').css({
-    transform:'translateX(-200%)'
+  $(".images>img:nth-child(3)").removeClass().addClass("leave")
+  .one('transitionend',function(x) {
+    $('x.currentTarget').removeClass().addClass('enter')
   })
-  $('.images>img:nth-child(1)').css({
-    transform:'translateX(-100%)'
-  })
-  $('.images>img:nth-child(3)').one('transitionend',function(x) {
-    $(x.currentTarget).addClass('right').css({transform:'none'})
-  })
-}, 6000);
 
-setTimeout(() => {
-  $('.images>img:nth-child(1)').css({
-    transform:'translateX(-200%)'
-  })
-  $('.images>img:nth-child(2)').css({
-    transform:'translateX(-100%)'
-  })
-  $('.images>img:nth-child(1)').one('transitionend',function(x) {
-    $(x.currentTarget).addClass('right').css({transform:'none'})
-  })
-}, 8000);
+  $(".images>img:nth-child(1)").removeClass().addClass("current")
+}, 6000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
